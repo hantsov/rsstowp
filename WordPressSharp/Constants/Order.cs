1ffd33445b98add7cf9fb2bbd30a1060af409ecf
Hardi Antsov <hardiantsov2@msn.com>
2016-03-22 10:25:00 +0200

    Added WordPessSharp directly as a project.

---
+++ WordPressSharp/Constants/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace WordPressSharp.Constants
+{
+	/// <summary>
+	/// Constats for ordering.
+	/// </summary>
+	public static class Order
+	{
+		public static readonly string Ascending = "ASC";
+		public static readonly string Descending = "DESC";
+	}
+}
